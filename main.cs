165ff742f5465bb34c9d168e1f91f6877fdf8c45
PierMario Salis <piermariosalis@MacBook-Pro-di-PierMario.fritz.box>
2021-07-16 16:47:44 +0200

    exercise 02 - determine which of two numbers is larger

---
+++ main.cs
@@ -0,0 +1,11 @@
+// determine which of two numbers is larger.
+
+int numberOne = 1500;
+
+int numberTwo = 1200;
+
+Console.WriteLine($"{numberOne} - {numberTwo}");
+
+int largerValue = Math.Max(numberOne, numberTwo);
+
+Console.WriteLine($"The larger number is : {largerValue}");
